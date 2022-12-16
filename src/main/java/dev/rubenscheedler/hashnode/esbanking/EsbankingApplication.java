7f1ab48183e744a851099b0250a43e95cc417592
ruben <scheedler.r@tkppensioen.nl>
2022-12-16 10:41:04 +0100

    Initial commit

---
+++ src/main/java/dev/rubenscheedler/hashnode/esbanking/EsbankingApplication.java
@@ -0,0 +1,13 @@
+package dev.rubenscheedler.hashnode.esbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rubenscheedler/hashnode/esbanking/EsbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rubenscheedler.hashnode.esbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
