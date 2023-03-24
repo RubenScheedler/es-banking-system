c87d4f3e0d28f6c78c950695942d0f3d8a2b4af6
ruben <scheedler.r@tkppensioen.nl>
2023-03-24 11:57:51 +0100

    Add NonNulls to CashDepositedEvent

--- src/main/java/dev/hashnode/rubenscheedler/esbanking/core/domain/command/events/CashDepositedEvent.java
+++ src/main/java/dev/hashnode/rubenscheedler/esbanking/core/domain/command/events/CashDepositedEvent.java
@@ -2,6 +2,7 @@ package dev.hashnode.rubenscheedler.esbanking.core.domain.command.events;
 
 import dev.hashnode.rubenscheedler.esbanking.core.domain.model.value.Money;
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Value;
 
 import java.util.UUID;
@@ -9,6 +10,8 @@ import java.util.UUID;
 @Value
 @Builder
 public class CashDepositedEvent {
+    @NonNull
     UUID accountId;
+    @NonNull
     Money amount;
 }
